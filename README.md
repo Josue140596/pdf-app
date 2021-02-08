9c704af0691e3e825083674af2d181f99b38718b
Josue140596 <josue140596@gmail.com>
2021-02-07 18:39:31 -0600

    loading

--- README.md
+++
@@ -1,9 +0,0 @@
-# Creating PDF :octocat: :file_folder:
-
-- This app create pdfs.
-- You can fill out a form with your personal information.
-
-
-
-
-
--- src/components/Form_pdf.js
+++ src/components/Form_pdf.js
@@ -1,46 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 // axios
 import axios from 'axios';
 // file-server
 
 import { saveAs } from 'file-saver';
 
-import { Form, Input, Button} from 'antd/lib';
+import { Form, Input, Button } from 'antd/lib';
 
 import { UserOutlined, HomeOutlined, PhoneOutlined, SolutionOutlined } from '@ant-design/icons';
 
 
-const createPDF = async (values) => {
-  
-    await axios.post('https://backend-pdf.herokuapp.com/create-pdf/temp', values)
-    .then((res)=>{
-        // console.log(res);
-    });
-  
-    await axios({
-        url: 'https://backend-pdf.herokuapp.com/create-pdf/send',
-        method: "GET",
-        responseType: "blob", 
-       
-      }).then((response) => {
-
-          let pdfBlob = new Blob([response.data], {type:'application/pdf'});
-          
-        saveAs(pdfBlob, "account.pdf") 
-        
-        
-      });
-}
 
 
 
 export const FormPdf = () => {
 
 
+    const [pdfCreated, setPdfCreated] = useState(false);
+
+    const createPDF = async (values) => {
+
+
+        setPdfCreated(true);
+        await axios.post('https://backend-pdf.herokuapp.com/create-pdf/temp', values)
+            .then((res) => {
+                // console.log(res);
+            });
+
+        await axios({
+            url: 'https://backend-pdf.herokuapp.com/create-pdf/send',
+            method: "GET",
+            responseType: "blob",
+
+        }).then((response) => {
+            let pdfBlob = new Blob([response.data], { type: 'application/pdf' });
+
+            saveAs(pdfBlob, "account.pdf")
+            setPdfCreated(false);
+
+        });
+    }
+
+
+
+
 
     return (
+
+
+
         <div className='sub-content-form'>
-            <Form
+
+            {pdfCreated
+
+                ? <div className='content-loading'>
+                    <p className='loading-text'>Creating PDF</p>
+                </div>
+                :  <Form
                 name="normal_login"
                 className="login-form"
                 initialValues={{ remember: true }}
@@ -59,11 +75,11 @@ export const FormPdf = () => {
                     rules={[{ required: true, message: 'Please input your Address!' }]}
                 >
                     <Input
-                        prefix={<HomeOutlined  className="site-form-item-icon" />}
+                        prefix={<HomeOutlined className="site-form-item-icon" />}
                         placeholder="Address"
                     />
                 </Form.Item>
-               
+
 
                 <Form.Item
                     name="accountN"
@@ -71,7 +87,7 @@ export const FormPdf = () => {
                     rules={[{ required: true, message: 'Please input your Account Number!' }]}
                 >
                     <Input
-                        prefix={<SolutionOutlined  className="site-form-item-icon" />}
+                        prefix={<SolutionOutlined className="site-form-item-icon" />}
                         placeholder="Account Number"
                     />
                 </Form.Item>
@@ -82,19 +98,24 @@ export const FormPdf = () => {
                     rules={[{ required: true, message: 'Please input your phone!' }]}
                 >
                     <Input
-                        prefix={<PhoneOutlined  className="site-form-item-icon" />}
+                        prefix={<PhoneOutlined className="site-form-item-icon" />}
                         placeholder="Phone"
                     />
                 </Form.Item>
-               
+
 
                 <Form.Item>
                     <Button type="primary" htmlType="submit" className="login-form-button">
                         Create PDF
                      </Button>
-     
+
                 </Form.Item>
             </Form>
+            }
+
+
+             
+            
         </div>
 
     )
